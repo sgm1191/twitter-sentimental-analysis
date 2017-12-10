b494d4719d1de515b39e70797c9b8f4dbd7e7426
santiago gonzalez medellin <santiago.gm1191@gmail.com>
2017-12-10 16:07:06 -0600

    word2vec con nce

---
+++ w2v/data/normalize.py
@@ -0,0 +1,52 @@
+import re
+
+# SCRIPT PARA PREPROCESAR TWITS
+# normalizar URLs, usuarios y hashtags
+
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+
+filename = 'cleansed_150k.ds'
+with open(filename) as f:
+    for line in f:
+        new_line = []
+        for word in preprocess(line, lowercase=True):
+            if word.startswith('@'):
+                new_line += ['<USER>']
+            elif word.startswith('http'):
+                new_line += ['<URL>']
+            elif word.startswith('#'):
+                new_line += ['<HASHTAG>']
+            else:
+                new_line += [word]
+        print(' '.join(new_line))
